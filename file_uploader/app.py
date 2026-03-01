86e23fd80461bdce743ffaef7ab43fe163f45fdd
Elysson MR <elyssonmr@gmail.com>
2026-02-28 21:35:08 -0300

    Add upload file endpoint

--- file_uploader/app.py
+++ file_uploader/app.py
@@ -3,16 +3,17 @@ import logging
 from fastapi import APIRouter, FastAPI
 
 from file_uploader.settings import Settings
+from file_uploader.upload.routes import router as upload_router
 
 logging.basicConfig(level=logging.INFO)
 settings = Settings()
 
 app = FastAPI()
 v1_apirouter = APIRouter(prefix='/v1')
-# v1_apirouter.include_router(YOUR_ROUTER)
+v1_apirouter.include_router(upload_router)
 app.include_router(v1_apirouter)
 
 
 @app.get('/health')
 def health_check():
-    return {'version': settings.version, 'message': 'ok'}
+    return {'version': settings.VERSION, 'message': 'ok'}
